c2855366ab433d36fde86e84dec657ecfc033628
Klyrasel <KaiLyrasel@outlook.com>
2022-09-12 17:38:02 -0400

    second commit

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ npc.walkNorth(1400, ()=>{
             npc.walkEast(1500, ()=>{
                 npc.walkSouth(1500,()=>{
                     npc.walkWest(2700, ()=>{
-                        
+                        npc.walkNorth(400, ()=>)
+
                     })
                 })
-            }
-        }
-    }
+            })
+        })
+    })
 
 // Create the inventory
 const inventory = newInventory()
